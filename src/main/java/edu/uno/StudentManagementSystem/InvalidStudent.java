73c235c9d4e01d052034add58dc3ce34e676159f
Arka Sahoo <arkasahoo2005@gmail.com>
2026-04-29 22:14:48 -0500

    Added Docstrings to InvalidStudent.java

--- src/main/java/edu/uno/StudentManagementSystem/InvalidStudent.java
+++ src/main/java/edu/uno/StudentManagementSystem/InvalidStudent.java
@@ -1,6 +1,14 @@
 package edu.uno.StudentManagementSystem;
 
+/**
+ * Thrown to indicate that a student object has invalid data
+ */
 public class InvalidStudent extends Exception {
+
+    /**
+     * Constructs an Exception
+     * @param message The detail message which explains the reason for the exception
+     */
     public InvalidStudent(String message) {
         super(message);
     }
